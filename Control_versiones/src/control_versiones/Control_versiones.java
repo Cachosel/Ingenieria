6a0e78236dd6b030bd94460cb9d30e54e82af171
Joaquin <Joaquin@Ratita.Home>
2015-10-30 11:27:02 -0300

    Primero commit

--- Control_versiones/src/control_versiones/Control_versiones.java
+++ Control_versiones/src/control_versiones/Control_versiones.java
@@ -6,6 +6,8 @@
 
 package control_versiones;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Joaquin
@@ -17,6 +19,8 @@ public class Control_versiones {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null,"Ejemplo de control de versiones");
     }
     
 }
